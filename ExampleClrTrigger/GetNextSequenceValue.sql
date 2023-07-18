79568d954d2638bd467824a8ff0d3cf4407a0c53
Stella Artois <officedev@gmx.ch>
2023-07-18 14:54:36 +0200

    Next sequence value

--- ExampleClrTrigger/GetNextSequenceValue.sql
+++ ExampleClrTrigger/GetNextSequenceValue.sql
@@ -5,19 +5,33 @@ RECONFIGURE;
 
 GO
 
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'compatibility')
+BEGIN
+    EXEC('CREATE SCHEMA compatibility; '); 
+END
 
-CREATE FUNCTION dbo.GetNextSequenceValue
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'compatibility.get_next_sequence_value') AND type IN (N'FN', N'IF', N'TF', N'FS', N'FT'))
+BEGIN
+    EXECUTE(N'
+CREATE FUNCTION compatibility.get_next_sequence_value
 (
-    @method national character varying(MAX) 
-) RETURNS bigint  
-AS EXTERNAL NAME DatabaseVersionControl.[DatabaseVersionControl.SequenceValueGetter].GetNextSequenceValue
-;
+    @method NVARCHAR(256)
+) 
+RETURNS BIGINT
+AS 
+EXTERNAL NAME DatabaseVersionControl.[DatabaseVersionControl.SequenceValueGetter].GetNextSequenceValue
+; ' );
+
+    PRINT 'Function "compatibility.get_next_sequence_value" created successfully.'
+END
 
 
 GO 
 
 
-DROP FUNCTION dbo.GetNextSequenceValue; 
+DROP FUNCTION compatibility.get_next_sequence_value; 
 
 
 GO 
