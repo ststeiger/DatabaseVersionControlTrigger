7f5fe92f7602b5f2c1acb95954874c3f35492d28
Stella Artois <officedev@gmx.ch>
2023-07-19 14:12:13 +0200

    HighestValue

--- DatabaseVersionControl/SequenceValueGetter.cs
+++ DatabaseVersionControl/SequenceValueGetter.cs
@@ -17,8 +17,8 @@ namespace DatabaseVersionControl
 
 
         [Microsoft.SqlServer.Server.SqlFunction(DataAccess = Microsoft.SqlServer.Server.DataAccessKind.Read)]
-        public static System.Data.SqlTypes.SqlInt64 GetNextSequenceValue( 
-            System.Data.SqlTypes.SqlString name 
+        public static System.Data.SqlTypes.SqlInt64 GetNextSequenceValue(
+            System.Data.SqlTypes.SqlString name
         )
         {
             object result = null;
@@ -29,7 +29,7 @@ namespace DatabaseVersionControl
                 bool haveToOpen = connection.State != System.Data.ConnectionState.Open;
                 if (haveToOpen)
                     connection.Open();
-                
+
                 using (System.Data.Common.DbCommand cmd = connection.CreateCommand())
                 {
                     cmd.CommandText = "SELECT NEXT VALUE FOR " + QuoteObject(name.Value) + " AS seq ";
@@ -40,8 +40,44 @@ namespace DatabaseVersionControl
                     connection.Close();
             } // End Using connection 
 
+            if (result == null || result == System.DBNull.Value)
+                return new System.Data.SqlTypes.SqlInt64(0);
+
             return new System.Data.SqlTypes.SqlInt64((long)result);
         } // End Function GetNextSequenceValue 
+
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = Microsoft.SqlServer.Server.DataAccessKind.Read)]
+        public static System.Data.SqlTypes.SqlInt64 GetHighestValue(
+            System.Data.SqlTypes.SqlString schema_name,
+            System.Data.SqlTypes.SqlString table_name,
+            System.Data.SqlTypes.SqlString column_name 
+        )
+        {
+            object result = null;
+
+            using (System.Data.Common.DbConnection connection =
+                 new System.Data.SqlClient.SqlConnection("context connection=true"))
+            {
+                bool haveToOpen = connection.State != System.Data.ConnectionState.Open;
+                if (haveToOpen)
+                    connection.Open();
+
+                using (System.Data.Common.DbCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT MAX("+ QuoteObject(column_name.Value) + ") AS val FROM " + QuoteObject(schema_name.Value) + "." + QuoteObject(table_name.Value) + "; ";
+                    result = cmd.ExecuteScalar();
+                } // End Using cmd 
+
+                if (haveToOpen && connection.State != System.Data.ConnectionState.Closed)
+                    connection.Close();
+            } // End Using connection 
+
+            // if (result == null || result == System.DBNull.Value)
+                // return new System.Data.SqlTypes.SqlInt64(0);
+
+            return new System.Data.SqlTypes.SqlInt64((long)result);
+        } // End Function GetHighestValue 
 
 
     } // End Class SequenceValueGetter 
